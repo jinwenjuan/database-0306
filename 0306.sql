61ef1a6af85df71abbed86177340d0dcae0d9aad
thu <mingfei.net@gmail.com>
2019-03-06 16:30:17 +0800

    Update

--- 0306.sql
+++ 0306.sql
@@ -1,25 +1,29 @@
 -- SQL 命令式
 # Ctrl + Enter
-show databases ;
-
+show databases;
 create database db_school;
-
 show tables from db_school;
-
-create table db_school.student(
+# use db_school;
+create table db_school.student
+(
   -- 字段名 数据类型 [数据范围] [约束] 注释,
-  name varchar(20) comment '学生姓名',
-  age int(2) comment '年龄'
+  name varchar(255) comment '学生姓名',
+  age  int(2) comment '年龄',
+  height double(3,2) comment '身高 x.yz 米',
+  dob date comment '出生年月日 Date Of Birth'
 );
 
-select * from db_school.student;
+show full columns from db_school.student;
 
-insert into db_school.student value ('Tom', 19);
-
-update db_school.student set age = 20;
-
-delete from db_school.student;
-
+select *
+from db_school.student;
+insert into db_school.student value ('Tom', 19, 1.75, '2000-01-23');
+update db_school.student
+set age = 20;
+delete
+from db_school.student;
 drop table db_school.student;
+DROP DATABASE db_school;
 
-drop database db_school;
+select 1 /*hello
+ mysql*/ + 1;
