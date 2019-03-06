bafacbb8689a74223c41258931e3c742fcbab778
thu <mingfei.net@gmail.com>
2019-03-06 16:51:27 +0800

    Update

--- 0306.sql
+++ 0306.sql
@@ -7,17 +7,21 @@ show tables from db_school;
 create table db_school.student
 (
   -- 字段名 数据类型 [数据范围] [约束] 注释,
+  sno varchar(255) unique comment '学号',
   name varchar(255) comment '学生姓名',
   age  int(2) comment '年龄',
-  height double(3,2) comment '身高 x.yz 米',
+  height double(3,2) not null comment '身高 x.yz 米',
   dob date comment '出生年月日 Date Of Birth'
-);
+)comment '学生表';
 
 show full columns from db_school.student;
 
 select *
 from db_school.student;
-insert into db_school.student value ('Tom', 19, 1.75, '2000-01-23');
+insert into db_school.student value ('001','Tom', 19, 1.75, '2000-01-23');
+insert into db_school.student value ('002','Jerry', 18, 1.65, '2001-01-23');
+# insert into db_school.student value ('Tester', 18, null, '2001-01-23');
+
 update db_school.student
 set age = 20;
 delete
