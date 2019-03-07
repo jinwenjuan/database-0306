1dbedfea8051e33d88d2a4cf271dca037e72386d
thu <mingfei.net@gmail.com>
2019-03-07 09:15:13 +0800

    Update

--- 0307.sql
+++ 0307.sql
@@ -1,24 +1,61 @@
 show tables from db_school;
 
-drop table db_school.student;
+drop database if exists db_school;
+create database db_school;
+
+drop table if exists db_school.student;
 create table db_school.student
 (
-  id     int auto_increment primary key comment 'id PK',
+  id           int auto_increment primary key comment 'id PK',
   -- 字段名 数据类型 [数据范围] [约束] 注释,
-  sno    varchar(255) unique comment '学号',
-  name   varchar(255) comment '学生姓名',
-  age    int(2) comment '年龄',
-  height double(3, 2) not null comment '身高 x.yz 米',
-  dob    date comment '出生年月日 Date Of Birth'
+  sno          varchar(255) unique comment '学号',
+  name         varchar(255) comment '学生姓名',
+  age          int(2) comment '年龄',
+  height       double(3, 2) not null comment '身高 x.yz 米',
+  dob          date comment '出生年月日 Date Of Birth',
+  departmentId int comment '院系 id'
 ) comment '学生表';
+
+drop table if exists db_school.department;
+create table db_school.department
+(
+  id    int auto_increment primary key comment 'id PK',
+  title varchar(255) comment '院系名称',
+  tel   varchar(255) comment '电话'
+) comment '院系表';
+
+-- 为学生表追加外键约束
+alter table db_school.student
+  add constraint
+    student_fk_departmentId
+    foreign key (departmentId)
+      references db_school.department (id);
 
 select *
 from db_school.student;
 
 desc db_school.student; -- desc: describe
 
-insert into db_school.student value (null, '006', 'Tom', 19, 1.7, '2000-1-1');
+insert into db_school.student value (null, '006', 'Tom', 19, 1.7, '2000-1-1', 1);
+insert into db_school.student value (null, '007', 'Jerry', 19, 1.7, '2000-1-1', 2);
+insert into db_school.student value (null, '008', 'Tester', 19, 1.7, '2000-1-1', 3);
+
 
 update db_school.student
 set dob = '2000-3-1'
 where name = 'tom';
+
+select *
+from db_school.department;
+
+select *
+from db_school.student;
+
+insert into db_school.department value (null, 'CS', '010-12345678');
+insert into db_school.department value (null, 'SS', '010-12345679');
+insert into db_school.department value (null, 'EE', '010-12345680');
+
+select s.name, d.title
+from db_school.student s
+       inner join db_school.department d
+                  on d.id = s.departmentId;
