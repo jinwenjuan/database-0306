9395b18208215dd6d30c42c7f11d43e65f6529bb
thu <mingfei.net@gmail.com>
2019-03-07 11:04:26 +0800

    Update

--- 0307.sql
+++ 0307.sql
@@ -9,12 +9,15 @@ create table db_school.student
   id           int auto_increment primary key comment 'id PK',
   -- 字段名 数据类型 [数据范围] [约束] 注释,
   sno          varchar(255) unique comment '学号',
+  gender varchar(255) default 'male' comment '性别',
   name         varchar(255) comment '学生姓名',
   age          int(2) comment '年龄',
   height       double(3, 2) not null comment '身高 x.yz 米',
   dob          date comment '出生年月日 Date Of Birth',
   departmentId int comment '院系 id'
 ) comment '学生表';
+
+alter table db_school.student auto_increment = 10000;
 
 drop table if exists db_school.department;
 create table db_school.department
@@ -27,23 +30,29 @@ create table db_school.department
 -- 为学生表追加外键约束
 alter table db_school.student
   add constraint
-    student_fk_departmentId
+    student_fk_departmentId -- 外键别名 表名_fk_列名
     foreign key (departmentId)
       references db_school.department (id);
+#       on delete cascade; -- 级联删除
+#       on delete set null ; -- 级联置空
 
 select *
 from db_school.student;
 
 desc db_school.student; -- desc: describe
 
-insert into db_school.student value (null, '006', 'Tom', 19, 1.7, '2000-1-1', 1);
-insert into db_school.student value (null, '007', 'Jerry', 19, 1.7, '2000-1-1', 2);
-insert into db_school.student value (null, '008', 'Tester', 19, 1.7, '2000-1-1', 3);
+insert into db_school.student value (null, '006', 'male', 'Tom', 19, 1.7, '2000-1-1', 1);
+insert into db_school.student value (null, '007', null, 'Jerry', 19, 1.7, '2000-1-1', 1);
+insert into db_school.student(sno, name, age, height, dob, departmentId) value ('009', 'Tester', 19, 1.7, '2000-1-1', 1);
 
 
 update db_school.student
 set dob = '2000-3-1'
 where name = 'tom';
+
+update db_school.student
+set gender = 'female', name = 'Jerry'
+where id = 10000;
 
 select *
 from db_school.department;
@@ -55,7 +64,20 @@ insert into db_school.department value (null, 'CS', '010-12345678');
 insert into db_school.department value (null, 'SS', '010-12345679');
 insert into db_school.department value (null, 'EE', '010-12345680');
 
+delete
+from db_school.department
+where id = 1;
+
+
+
+
 select s.name, d.title
 from db_school.student s
        inner join db_school.department d
                   on d.id = s.departmentId;
+
+drop table db_school.department;
+drop table db_school.student;
+
+set foreign_key_checks = 0;
+set foreign_key_checks = 1;
