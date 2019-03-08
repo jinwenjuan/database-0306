8b6842e5488ab6a28bd81ff38d4e83d01ed2949a
thu <mingfei.net@gmail.com>
2019-03-08 14:16:43 +0800

    Update

--- 0308.sql
+++ 0308.sql
@@ -18,7 +18,8 @@ select *
 from scott.emp;
 
 
-select EMPNO, HIREDATE -- 投影
+select EMPNO,
+       HIREDATE -- 投影
 from scott.emp;
 
 select distinct JOB
@@ -26,10 +27,11 @@ from scott.emp;
 
 select *
 from scott.emp
-# where SAL > 2000;
-# where SAL <> 3000;
-# where SAL <> 3000 and JOB <> 'salesman';
-where SAL <> 3000 or JOB <> 'salesman';
+     # where SAL > 2000;
+     # where SAL <> 3000;
+     # where SAL <> 3000 and JOB <> 'salesman';
+where SAL <> 3000
+   or JOB <> 'salesman';
 
 -- ci: case insensitive
 select *
@@ -38,11 +40,11 @@ where ENAME = binary 'Scott';
 
 select JOB, ENAME, HIREDATE
 from scott.emp
-# order by ENAME asc; -- ascend default
-# order by ENAME desc; -- descend
-# order by ENAME;
-# order by HIREDATE; -- 工龄长的在前？后？
-order by JOb, ENAME desc ;
+     # order by ENAME asc; -- ascend default
+     # order by ENAME desc; -- descend
+     # order by ENAME;
+     # order by HIREDATE; -- 工龄长的在前？后？
+order by JOb, ENAME desc;
 
 
 select comm
@@ -52,32 +54,34 @@ order by COMM;
 
 select *
 from scott.emp
-limit 3 offset 0; -- offset 偏移量
+limit 3 offset 0;
+-- offset 偏移量
 # limit 0, 3;
 
 select ENAME
 from scott.emp
-# where ENAME like '_COTT';
+     # where ENAME like '_COTT';
 where ENAME like '%\_%' escape '\\';
 
 update scott.emp
-set ENAME = 'sco_tt' where ENAME = 'scott';
+set ENAME = 'sco_tt'
+where ENAME = 'scott';
 
 select *
 from scott.emp;
 
 select *
 from scott.emp
-# where JOB = 'salesman' or JOB = 'manager' or JOB = 'president';
-where job in ('salesman','manager','president');
+     # where JOB = 'salesman' or JOB = 'manager' or JOB = 'president';
+where job in ('salesman', 'manager', 'president');
 
 select *
 from scott.emp
-# where SAL >= 2000 and sal <=3000; -- [2000, 3000]
+     # where SAL >= 2000 and sal <=3000; -- [2000, 3000]
 where SAL between 2000 and 3000;
 
 
-select e.ename  as '员工 姓名'
+select e.ename as '员工 姓名'
 from scott.emp as e;
 
 select *
@@ -91,3 +95,14 @@ where EMPNO = '7499';-- DML
 select ENAME, sal + ifnull(comm, 0)
 from scott.emp;
 
+select *
+from scott.emp;
+
+select *
+from scott.dept;
+
+select e.ENAME, e.DEPTNO, d.DNAME, d.DEPTNO
+from scott.emp e
+       inner join scott.dept d
+on e.DEPTNO = d.DEPTNO;
+-- 52 = 13 * 4
