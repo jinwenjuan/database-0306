60c691afbefbe2a874c005df9a484fa288cbbd7a
thu <mingfei.net@gmail.com>
2019-03-08 16:46:14 +0800

    Update

--- ip.sql
+++ ip.sql
@@ -2,18 +2,30 @@ drop database if exists db_ip;
 create database db_ip;
 
 drop table if exists db_ip.ip;
-create table db_ip.ip(
-  id int auto_increment primary key comment 'id PK',
-  min varchar(15) not null comment 'min ip NN',
-  max varchar(15) not null comment 'max ip NN',
+create table db_ip.ip
+(
+  id  int auto_increment primary key comment 'id PK',
+  min varchar(255) not null comment 'min ip NN',
+  max varchar(255) not null comment 'max ip NN',
   loc varchar(255) not null comment 'location NN'
-)comment 'ip address table';
+) comment 'ip address table';
+
+load data local infile 'd:/ip.txt'
+  into table db_ip.ip
+  fields terminated by '|'
+  (min, max, loc)
+  set id = null;
 
 select *
 from db_ip.ip;
 
-load data local infile 'd:/ip.txt'
-into table db_ip.ip
-fields terminated by '|'
-  (min, max, loc)
-set id = null;
+select count(*)
+from db_ip.ip;
+
+update db_ip.ip
+set min = trim(min),
+    max = trim(max),
+    loc = trim(loc);
+
+-- 166.111.3.207
+-- SQL ?
