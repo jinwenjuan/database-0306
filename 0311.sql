2a65a50fe3fc09bb407012be4c50825051da3ec9
thu <mingfei.net@gmail.com>
2019-03-11 09:06:43 +0800

    Update

--- 0311.sql
+++ 0311.sql
@@ -22,3 +22,20 @@ where DEPTNO = 30;
 
 select *
 from scott.v_emp;
+
+select ENAME, DNAME
+from scott.emp e
+       inner join scott.dept d
+                  on e.DEPTNO = d.DEPTNO
+where ENAME = 'allen';
+
+create or replace view scott.v_name
+as
+select e.EMPNO, e.ENAME, d.DNAME
+from scott.emp e
+       inner join scott.dept d
+                  on e.DEPTNO = d.DEPTNO;
+
+select *
+from scott.v_name
+where ENAME = 'allen';
