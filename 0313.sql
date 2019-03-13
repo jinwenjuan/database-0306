946fb4b110876a25d71d36bcd344a927ebd08441
thu <mingfei.net@gmail.com>
2019-03-13 08:53:49 +0800

    Update

---
+++ 0313.sql
@@ -0,0 +1,14 @@
+/*
+db_school
+
+department
+id, title, tel,           mobile
+1,  CS,    010-12345678, 18612345678
+
+
+student
+id, sno, name, gender, age, deptId（FK）
+1, null, Tom,  male,   18,  1
+2, 002, Jerry, female, 18,  1
+3, null, Tom,  male,   18,  1
+*/
